e9af82519b160a8183e59120c8fbcb01e8221aa8
varundharne <varun11@s.amity.edu>
2025-06-01 23:15:03 +0530

    contribution by varun

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <cstring>
 using namespace std;
 
 class Account {
@@ -19,6 +20,7 @@ public:
     int retAccountNumber() const;
     int retDeposit() const;
     char retType() const;
+    const char* retName() const;
 };
 
 void Account::createAccount() {
@@ -54,30 +56,18 @@ void Account::modify() {
     cin >> deposit;
 }
 
-void Account::depositAmount(int x) {
-    deposit += x;
-}
-
-void Account::withdrawAmount(int x) {
-    deposit -= x;
-}
+void Account::depositAmount(int x) { deposit += x; }
+void Account::withdrawAmount(int x) { deposit -= x; }
 
 void Account::report() const {
     cout << accountNumber << setw(10) << " " << name << setw(10)
          << " " << type << setw(6) << deposit << endl;
 }
 
-int Account::retAccountNumber() const {
-    return accountNumber;
-}
-
-int Account::retDeposit() const {
-    return deposit;
-}
-
-char Account::retType() const {
-    return type;
-}
+int Account::retAccountNumber() const { return accountNumber; }
+int Account::retDeposit() const { return deposit; }
+char Account::retType() const { return type; }
+const char* Account::retName() const { return name; }
 
 // Function declarations
 void writeAccount();
@@ -86,6 +76,9 @@ void modifyAccount(int);
 void deleteAccount(int);
 void displayAll();
 void depositWithdraw(int, int);
+void searchAccountByName();
+void checkLoanEligibility(int);
+void applyForLoan(int);
 void intro();
 
 int main() {
@@ -103,41 +96,25 @@ int main() {
         cout << "\n\n\t06. CLOSE AN ACCOUNT";
         cout << "\n\n\t07. MODIFY AN ACCOUNT";
         cout << "\n\n\t08. EXIT";
-        cout << "\n\n\tSelect Your Option (1-8): ";
+        cout << "\n\n\t09. SEARCH ACCOUNT BY NAME";
+        cout << "\n\n\t10. CHECK LOAN ELIGIBILITY";
+        cout << "\n\n\t11. APPLY FOR LOAN";
+        cout << "\n\n\tSelect Your Option (1-11): ";
         cin >> ch;
 
         switch (ch) {
-        case '1':
-            writeAccount();
-            break;
-        case '2':
-            cout << "\n\n\tEnter The account No. : "; cin >> num;
-            depositWithdraw(num, 1);
-            break;
-        case '3':
-            cout << "\n\n\tEnter The account No. : "; cin >> num;
-            depositWithdraw(num, 2);
-            break;
-        case '4':
-            cout << "\n\n\tEnter The account No. : "; cin >> num;
-            displayAccount(num);
-            break;
-        case '5':
-            displayAll();
-            break;
-        case '6':
-            cout << "\n\n\tEnter The account No. : "; cin >> num;
-            deleteAccount(num);
-            break;
-        case '7':
-            cout << "\n\n\tEnter The account No. : "; cin >> num;
-            modifyAccount(num);
-            break;
-        case '8':
-            cout << "\n\n\tThanks for using bank management system!";
-            break;
-        default:
-            cout << "\a";
+        case '1': writeAccount(); break;
+        case '2': cout << "\n\n\tEnter The account No. : "; cin >> num; depositWithdraw(num, 1); break;
+        case '3': cout << "\n\n\tEnter The account No. : "; cin >> num; depositWithdraw(num, 2); break;
+        case '4': cout << "\n\n\tEnter The account No. : "; cin >> num; displayAccount(num); break;
+        case '5': displayAll(); break;
+        case '6': cout << "\n\n\tEnter The account No. : "; cin >> num; deleteAccount(num); break;
+        case '7': cout << "\n\n\tEnter The account No. : "; cin >> num; modifyAccount(num); break;
+        case '8': cout << "\n\n\tThanks for using bank management system!"; break;
+        case '9': searchAccountByName(); break;
+        case '10': cout << "\n\n\tEnter The account No. : "; cin >> num; checkLoanEligibility(num); break;
+        case '11': cout << "\n\n\tEnter The account No. : "; cin >> num; applyForLoan(num); break;
+        default: cout << "\a";
         }
         cin.ignore();
         cin.get();
@@ -147,128 +124,89 @@ int main() {
 
 void writeAccount() {
     Account ac;
-    ofstream outFile;
-    outFile.open("account.dat", ios::binary | ios::app);
+    ofstream outFile("account.dat", ios::binary | ios::app);
     ac.createAccount();
-    outFile.write(reinterpret_cast<char *> (&ac), sizeof(Account));
+    outFile.write(reinterpret_cast<char*>(&ac), sizeof(Account));
     outFile.close();
 }
 
 void displayAccount(int n) {
     Account ac;
     bool flag = false;
-    ifstream inFile;
-    inFile.open("account.dat", ios::binary);
-    if (!inFile) {
-        cout << "File could not be open !! Press any Key...";
-        return;
-    }
-
-    while (inFile.read(reinterpret_cast<char *> (&ac), sizeof(Account))) {
+    ifstream inFile("account.dat", ios::binary);
+    while (inFile.read(reinterpret_cast<char*>(&ac), sizeof(Account))) {
         if (ac.retAccountNumber() == n) {
             ac.showAccount();
             flag = true;
         }
     }
+    if (!flag) cout << "\n\nAccount number does not exist";
     inFile.close();
-    if (!flag)
-        cout << "\n\nAccount number does not exist";
 }
 
 void modifyAccount(int n) {
-    bool found = false;
     Account ac;
-    fstream File;
-    File.open("account.dat", ios::binary | ios::in | ios::out);
-    if (!File) {
-        cout << "File could not be open !! Press any Key...";
-        return;
-    }
-
-    while (!File.eof() && found == false) {
+    fstream File("account.dat", ios::binary | ios::in | ios::out);
+    bool found = false;
+    while (!File.eof() && !found) {
         int pos = File.tellg();
-        File.read(reinterpret_cast<char *> (&ac), sizeof(Account));
+        File.read(reinterpret_cast<char*>(&ac), sizeof(Account));
         if (ac.retAccountNumber() == n) {
             ac.showAccount();
             cout << "\n\nEnter The New Details of account" << endl;
             ac.modify();
             File.seekp(pos);
-            File.write(reinterpret_cast<char *> (&ac), sizeof(Account));
+            File.write(reinterpret_cast<char*>(&ac), sizeof(Account));
             cout << "\n\n\t Record Updated";
             found = true;
         }
     }
     File.close();
-    if (!found)
-        cout << "\n\n Record Not Found ";
+    if (!found) cout << "\n\n Record Not Found ";
 }
 
 void deleteAccount(int n) {
     Account ac;
-    ifstream inFile;
-    ofstream outFile;
-    inFile.open("account.dat", ios::binary);
-    if (!inFile) {
-        cout << "File could not be open !! Press any Key...";
-        return;
+    ifstream inFile("account.dat", ios::binary);
+    ofstream outFile("Temp.dat", ios::binary);
+    while (inFile.read(reinterpret_cast<char*>(&ac), sizeof(Account))) {
+        if (ac.retAccountNumber() != n)
+            outFile.write(reinterpret_cast<char*>(&ac), sizeof(Account));
     }
-    outFile.open("Temp.dat", ios::binary);
-    inFile.seekg(0, ios::beg);
-    while (inFile.read(reinterpret_cast<char *> (&ac), sizeof(Account))) {
-        if (ac.retAccountNumber() != n) {
-            outFile.write(reinterpret_cast<char *> (&ac), sizeof(Account));
-        }
-    }
-    inFile.close();
-    outFile.close();
-    remove("account.dat");
-    rename("Temp.dat", "account.dat");
+    inFile.close(); outFile.close();
+    remove("account.dat"); rename("Temp.dat", "account.dat");
     cout << "\n\n\tRecord Deleted ..";
 }
 
 void displayAll() {
     Account ac;
-    ifstream inFile;
-    inFile.open("account.dat", ios::binary);
-    if (!inFile) {
-        cout << "File could not be open !! Press any Key...";
-        return;
-    }
+    ifstream inFile("account.dat", ios::binary);
     cout << "\n\n\t\tACCOUNT HOLDER LIST\n\n";
     cout << "====================================================\n";
     cout << "A/c no.      NAME           Type  Balance\n";
     cout << "====================================================\n";
-    while (inFile.read(reinterpret_cast<char *> (&ac), sizeof(Account))) {
+    while (inFile.read(reinterpret_cast<char*>(&ac), sizeof(Account))) {
         ac.report();
     }
     inFile.close();
 }
 
 void depositWithdraw(int n, int option) {
-    int amt;
-    bool found = false;
     Account ac;
-    fstream File;
-    File.open("account.dat", ios::binary | ios::in | ios::out);
-    if (!File) {
-        cout << "File could not be open !! Press any Key...";
-        return;
-    }
-
-    while (!File.eof() && found == false) {
+    fstream File("account.dat", ios::binary | ios::in | ios::out);
+    bool found = false;
+    while (!File.eof() && !found) {
         int pos = File.tellg();
-        File.read(reinterpret_cast<char *> (&ac), sizeof(Account));
+        File.read(reinterpret_cast<char*>(&ac), sizeof(Account));
         if (ac.retAccountNumber() == n) {
             ac.showAccount();
+            int amt;
             if (option == 1) {
-                cout << "\n\n\tTO DEPOSIT AMOUNT ";
-                cout << "\n\nEnter The amount to be deposited: ";
+                cout << "\n\nEnter amount to deposit: ";
                 cin >> amt;
                 ac.depositAmount(amt);
-            }
-            if (option == 2) {
-                cout << "\n\n\tTO WITHDRAW AMOUNT ";
-                cout << "\n\nEnter The amount to be withdrawn: ";
+            } else {
+                cout << "\n\nEnter amount to withdraw: ";
                 cin >> amt;
                 int bal = ac.retDeposit() - amt;
                 if ((bal < 500 && ac.retType() == 'S') || (bal < 1000 && ac.retType() == 'C'))
@@ -277,14 +215,78 @@ void depositWithdraw(int n, int option) {
                     ac.withdrawAmount(amt);
             }
             File.seekp(pos);
-            File.write(reinterpret_cast<char *> (&ac), sizeof(Account));
+            File.write(reinterpret_cast<char*>(&ac), sizeof(Account));
             cout << "\n\n\t Record Updated";
             found = true;
         }
     }
     File.close();
-    if (!found)
-        cout << "\n\n Record Not Found ";
+    if (!found) cout << "\n\n Record Not Found ";
+}
+
+void searchAccountByName() {
+    char searchName[50];
+    Account ac;
+    bool found = false;
+    ifstream inFile("account.dat", ios::binary);
+    cout << "\n\nEnter the name to search: ";
+    cin.ignore();
+    cin.getline(searchName, 50);
+    while (inFile.read(reinterpret_cast<char*>(&ac), sizeof(Account))) {
+        if (stricmp(ac.retName(), searchName) == 0) {
+            ac.showAccount();
+            found = true;
+        }
+    }
+    if (!found) cout << "\n\nNo account found with that name!";
+    inFile.close();
+}
+
+void checkLoanEligibility(int n) {
+    Account ac;
+    bool found = false;
+    ifstream inFile("account.dat", ios::binary);
+    while (inFile.read(reinterpret_cast<char*>(&ac), sizeof(Account))) {
+        if (ac.retAccountNumber() == n) {
+            ac.showAccount();
+            found = true;
+            int balance = ac.retDeposit();
+            char type = ac.retType();
+            if ((type == 'S' && balance >= 10000) || (type == 'C' && balance >= 20000))
+                cout << "\n\n✅ Eligible for loan.";
+            else
+                cout << "\n\n❌ Not eligible for loan.";
+        }
+    }
+    if (!found) cout << "\n\nAccount number does not exist.";
+    inFile.close();
+}
+
+void applyForLoan(int n) {
+    Account ac;
+    bool found = false;
+    ifstream inFile("account.dat", ios::binary);
+    while (inFile.read(reinterpret_cast<char*>(&ac), sizeof(Account))) {
+        if (ac.retAccountNumber() == n) {
+            found = true;
+            cout << "\n\nApplying for Loan:\n";
+            ac.showAccount();
+            int balance = ac.retDeposit();
+            char type = ac.retType();
+            if ((type == 'S' && balance >= 10000) || (type == 'C' && balance >= 20000)) {
+                double amount;
+                cout << "\n\nStatus: ✅ Eligible for loan.";
+                cout << "\nEnter loan amount to apply: ₹";
+                cin >> amount;
+                cout << "\nLoan of ₹" << amount << " applied successfully. Bank will contact you soon.";
+            } else {
+                cout << "\n\nStatus: ❌ Not eligible for loan.";
+            }
+            break;
+        }
+    }
+    inFile.close();
+    if (!found) cout << "\n\nAccount number does not exist.";
 }
 
 void intro() {
@@ -295,4 +297,3 @@ void intro() {
     cout << "\n\nSCHOOL : Demo School\n";
     cin.get();
 }
-
